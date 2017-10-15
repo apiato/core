8d5c6156506984ad57f220775e4aea84179aede3
Mahmoud Zalt <mahmoud@zalt.me>
2017-10-15 19:52:21 +0300

    refactor exception related classes

---
+++ Exceptions/Handlers/ExceptionsHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Apiato\Core\Exceptions\Handlers;
+
+use Optimus\Heimdal\ExceptionHandler as HeimdalExceptionHandler;
+
+/**
+ * Class ExceptionsHandler
+ *
+ * @author  Mahmoud Zalt  <mahmoud@zalt.me>
+ */
+class ExceptionsHandler extends HeimdalExceptionHandler
+{
+
+}
