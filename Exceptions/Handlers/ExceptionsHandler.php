f7fb2a32f34dbefb2291da22a857683c6b7cb7ea
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-10-18 08:48:38 +0200

    fix WEB UI Exceptions

--- Exceptions/Handlers/ExceptionsHandler.php
+++ Exceptions/Handlers/ExceptionsHandler.php
@@ -2,7 +2,10 @@
 
 namespace Apiato\Core\Exceptions\Handlers;
 
+use Exception;
+use Illuminate\Support\Facades\Config;
 use Optimus\Heimdal\ExceptionHandler as HeimdalExceptionHandler;
+use Illuminate\Foundation\Exceptions\Handler as LaravelExceptionHandler;
 
 /**
  * Class ExceptionsHandler
@@ -11,5 +14,15 @@ use Optimus\Heimdal\ExceptionHandler as HeimdalExceptionHandler;
  */
 class ExceptionsHandler extends HeimdalExceptionHandler
 {
+    public function render($request, Exception $e)
+    {
+        // if the user expects json or the API forces the user to send it
+        if (($request->expectsJson()) || (Config::get('apiato.requests.force-accept-header'))) {
+            // return the error as json
+            return parent::render($request, $e);
+        }
 
+        // neither the user nor the application wants json
+        return LaravelExceptionHandler::render($request, $e);
+    }
 }
