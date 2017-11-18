5549356f3eeb8f75419dcaf2837c0631ea6430b3
Andriy Butnar <andriy.butnar@redentu.com>
2017-11-18 14:10:29 +0200

    Fix call extra methods with arguments bug

--- Traits/CallableTrait.php
+++ Traits/CallableTrait.php
@@ -122,38 +122,24 @@ trait CallableTrait
     private function callExtraMethods($class, $methods)
     {
         // allows calling other methods in the class before calling the main `run` function.
-        foreach ($methods as $methodInfo) {
-            // if is array means it method has arguments
-            if (is_array($methodInfo)) {
-                $this->callWithArguments($class, $methodInfo);
-            } else {
-                // if is string means it's just the method name without arguments
-                $this->callWithoutArguments($class, $methodInfo);
-            }
+        foreach ($methods as $methodName => $arguments) {
+
+            //make sure arguments is an array
+            $arguments = is_array($arguments) ? $arguments : [$arguments];
+
+            $this->callMethod($class, $methodName, $arguments);
         }
     }
 
     /**
      * @param $class
-     * @param $methodInfo
+     * @param $method
+     * @param $arguments
      */
-    private function callWithArguments($class, $methodInfo)
+    private function callMethod($class, $method, $arguments)
     {
-        $method = key($methodInfo);
-        $arguments = $methodInfo[$method];
         if (method_exists($class, $method)) {
             $class->$method(...$arguments);
-        }
-    }
-
-    /**
-     * @param $class
-     * @param $methodInfo
-     */
-    private function callWithoutArguments($class, $methodInfo)
-    {
-        if (method_exists($class, $methodInfo)) {
-            $class->$methodInfo();
         }
     }
 
