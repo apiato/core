6ca0efa13e78cf4c71efd8287672732d7257e674
Johannes Schobel <johannes.schobel@uni-ulm.de>
2017-11-29 11:00:36 +0100

    refactoring command

--- Commands/ListContainerDependenciesCommand.php
+++ Commands/ListContainerDependenciesCommand.php
@@ -11,7 +11,6 @@ use RecursiveIteratorIterator;
 use Spatie\Fractal\Fractal;
 use Spatie\Fractalistic\ArraySerializer;
 
-
 /**
  * Class FindContainerDependenciesCommand
  * Parses all files in the Container. This is needed due to the implemented apiato calls.
@@ -20,13 +19,12 @@ use Spatie\Fractalistic\ArraySerializer;
  *
  * @author Fabian Widmann <fabian.widmann@gmail.com>
  */
-class FindContainerDependenciesCommand extends ConsoleCommand
+class ListContainerDependenciesCommand extends ConsoleCommand
 {
 
     protected $signature = 'apiato:list:dependencies {containerPath}';
 
     protected $description = 'Lists all dependencies from the given container to other containers.';
-
 
     public function __construct()
     {
@@ -50,7 +48,6 @@ class FindContainerDependenciesCommand extends ConsoleCommand
             $this->info('Found dependencies:');
             $this->info($this->prettyPrintArray($fileContainerMatch));
 
-
             $input = $this->ask('Display Container author and description from the composer.json?(y/n)');
             if (isset($input) && $input == 'y') {
                 // $fileContainerMatch structure:
@@ -60,48 +57,59 @@ class FindContainerDependenciesCommand extends ConsoleCommand
                 $matches = array_unique(array_keys(array_merge(...array_values($fileContainerMatch))));
                 foreach ($matches as $match) {
                     $this->info($this->prettyPrintArray($this->getComposerInformation($match)));
-
                 }
             }
-        } else
+        } else {
             $this->info('No dependencies found.');
+        }
     }
 
-    /**Utility print function that takes an array and outputs it by applying the given indent. Each array found will be printed recursively with indent+indentmodifier.
-     * @param $arr
+    /**
+     * Utility print function that takes an array and outputs it by applying the given indent. Each array found will be printed recursively with indent+indentmodifier.
+     *
+     * @param     $arr
      * @param int $indent
      * @param int $indentModifier
+     *
      * @return string
+     * @throws \InvalidArgumentException
      */
     private function prettyPrintArray($arr, $indent = 0, $indentModifier = 4)
     {
         if (!is_array($arr)) {
             return $arr;
         }
+
         $string = "";
+
         foreach ($arr as $key => $value) {
-            $string .= str_repeat(" ", $indent) . "[" . $key . "]" . ": ";
+            $string = $string . str_repeat(" ", $indent) . "[" . $key . "]" . ": ";
+
             if (is_array($value)) {
                 $string .= PHP_EOL . $this->prettyPrintArray($value, $indent + $indentModifier) . PHP_EOL;
             } else if (is_string($value) || settype($item, 'string') !== false || (is_object($value) && method_exists($value, '__toString'))){
                 $string .= $value . PHP_EOL;
             }
-            else
+            else {
                 throw new \InvalidArgumentException('Current value cannot be converted to string: value=' . $value);
+            }
         }
         return $string;
     }
 
     /**
      * Get composer information by decoding the json and applying the ComposerTransformer.
+     *
      * @param $containerName
      * @return array|string
      */
     private function getComposerInformation($containerName)
     {
         $composerFile = 'app/Containers/' . $containerName . '/composer.json';
+
         try {
             $content = file_get_contents($composerFile);
+
             if (isset($content)) {
                 $json = \GuzzleHttp\json_decode($content);
                 return Fractal::create($json, ComposerTransformer::class, ArraySerializer::class)->toArray();
@@ -113,24 +121,28 @@ class FindContainerDependenciesCommand extends ConsoleCommand
 
     /**
      * Extracts the content of a file  and find all containers by finding all containers in App\Containers\$containerName\*
+     *
      * @param $filePath string - path to the file
      * @return null | array of containers
      */
     private function getContainerFromUseStatement($filePath)
     {
         $content = file_get_contents($filePath);
-        //alphanumeric?
+
+        // is the containername alphanumeric?
         preg_match_all('/use App\\\\Containers\\\\(?P<containers>[a-zA-Z\d]*)\\\\/', $content, $matches);
         $ret = [];
+
         if (isset($matches['containers'])) {
             $ret['containers'] = array_unique($matches['containers']);
         }
+
         return $ret;
     }
 
-
     /**
      * Extracts the content of a file  and find all containers by finding all containers in App\Containers\$containerName\*
+     *
      * @param $filePath string - path to the file
      * @return null | array of containers
      */
@@ -146,6 +158,7 @@ class FindContainerDependenciesCommand extends ConsoleCommand
         $pattern = "/^([^\/\/]*|[^\/\*]*)Apiato::call\('(?P<containers>.*?)@([?P<functions>a-zA-Z][a-zA-Z\d]*?)',.*?\[(?P<args>.*?)]/m";
         preg_match_all($pattern, $content, $matches);
         $ret = [];
+
         if (isset($matches['containers'])) {
             $ret['containers'] = array_unique($matches['containers']);
         }
@@ -157,13 +170,18 @@ class FindContainerDependenciesCommand extends ConsoleCommand
      * Iterates through the given path recursively to obtain
      *  1) all used containers of the given container
      *  2) an array that contains the containers as keys and all files using it as value.
-     * @param $path - to the container
+     *
+     * @param      $path - to the container
+     * @param bool $filterOwnContainer
+     *
      * @return array - [$usedContainers, $filesInContainers]
+     * @throws InvalidPathException
      */
     private function getDependencies($path, $filterOwnContainer = false)
     {
         $ownContainerName = explode('/', explode('app/Containers/', $path)[1])[0];
-        if (!file_exists($path)){
+
+        if (!file_exists($path)) {
             throw new InvalidPathException('Given path does not exist: path=' . $path);
         }
 
@@ -171,30 +189,32 @@ class FindContainerDependenciesCommand extends ConsoleCommand
         $useStatements = [];
         $filesInContainers = [];
 
-        foreach ($recursiveIteratorIterator as $file)
+        foreach ($recursiveIteratorIterator as $file) {
             if (!$file->isDir()) {
                 $apiatoCalls = $this->getContainerFromApiatoCall($file->getPathName());
                 $imports = $this->getContainerFromUseStatement($file->getPathName());
 
                 if (isset($apiatoCalls['containers'])) {
-                    if ($filterOwnContainer){
+                    if ($filterOwnContainer) {
                         $apiatoCalls['containers'] = array_diff($apiatoCalls['containers'], [$ownContainerName]);
                     }
+
                     foreach ($apiatoCalls['containers'] as $container) {
                         $filesInContainers['apiatoCalls'][$container][] = $file->getPathName();
                     }
                 }
 
                 if (isset($imports['containers'])) {
-                    if ($filterOwnContainer){
+                    if ($filterOwnContainer) {
                         $imports['containers'] = array_diff($imports['containers'], [$ownContainerName]);
                     }
+
                     foreach ($imports['containers'] as $container) {
                         $filesInContainers['imports'][$container][] = $file->getPathName();
                     }
-
                 }
             }
+        }
 
         return $filesInContainers;
     }
