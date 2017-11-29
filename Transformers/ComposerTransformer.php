e8f15c411eb8626f21c9d2fa5284857135f8fda2
Johannes Schobel <johannesschobel@users.noreply.github.com>
2017-11-29 11:01:18 +0100

    refactoring command (#53)

--- Transformers/ComposerTransformer.php
+++ Transformers/ComposerTransformer.php
@@ -2,28 +2,35 @@
 
 namespace Apiato\Core\Transformers;
 
-
 use Apiato\Core\Abstracts\Transformers\Transformer;
 use stdClass;
 
-
+/**
+ * Class ComposerTransformer
+ *
+ * @author Fabian Widmann <fabian.widmann@gmail.com>
+ */
 class ComposerTransformer extends Transformer
 {
-
     /**
      * @param stdClass $decodedJson
      * @return array
      */
     public function transform(stdClass $decodedJson)
     {
-        $ret = [
+        $result = [
             'name' => $decodedJson->name,
             'description' => $decodedJson->name,
         ];
-        if (isset($decodedJson->type))
-            $ret['type'] = $decodedJson->type;
-        if (isset($decodedJson->support))
-            $ret['support'] = (array)$decodedJson->support;
-        return $ret;
+
+        if (isset($decodedJson->type)) {
+            $result['type'] = $decodedJson->type;
+        }
+
+        if (isset($decodedJson->support)) {
+            $result['support'] = (array)$decodedJson->support;
+        }
+
+        return $result;
     }
 }
