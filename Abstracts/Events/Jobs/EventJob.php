822d54b7c4b28e7a68d9cfafaa9e3a620292473c
Arthur Noxious <aamining2@gmail.com>
2018-08-13 09:33:14 +0200

    Update EventJob.php

--- Abstracts/Events/Jobs/EventJob.php
+++ Abstracts/Events/Jobs/EventJob.php
@@ -4,34 +4,41 @@ namespace Apiato\Core\Abstracts\Events\Jobs;
 
 use Apiato\Core\Abstracts\Events\Interfaces\ShouldHandle;
 use Apiato\Core\Abstracts\Jobs\Job;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 /**
  * Class EventJob
  *
  * @author  Arthur Devious
  */
-class EventJob extends Job
+class EventJob extends Job implements ShouldQueue
 {
-    public $handler;
+  use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * EventJob constructor.
-     *
-     * @param \Apiato\Core\Abstracts\Events\Interfaces\ShouldHandle $handler
-     */
+  public $handler;
 
-    public function __construct(ShouldHandle $handler)
-    {
-        $this->handler = $handler;
-    }
+  /**
+   * EventJob constructor.
+   *
+   * @param \Apiato\Core\Abstracts\Events\Interfaces\ShouldHandle $handler
+   */
 
-    /**
-     * Handle the job.
-     *
-     * @return void
-     */
-    public function handle()
-    {
-        $this->handler->handle();
-    }
+  public function __construct(ShouldHandle $handler)
+  {
+    $this->handler = $handler;
+  }
+
+  /**
+   * Handle the job.
+   *
+   * @return void
+   */
+  public function handle()
+  {
+    $this->handler->handle();
+  }
 }
