5e04c862e271d494f440c39c95993f0bac3f360a
Arthur Noxious <aamining2@gmail.com>
2018-08-13 16:27:43 +0200

    Update Dispatcher.php

--- Abstracts/Events/Dispatcher/Dispatcher.php
+++ Abstracts/Events/Dispatcher/Dispatcher.php
@@ -14,34 +14,39 @@ use Illuminate\Foundation\Bus\PendingDispatch as JobDispatcher;
  */
 class Dispatcher extends EventDispatcher
 {
-  public function dispatch($event, $payload = [], $halt = false)
-  {
-    /* Handle the event Async when the ShouldHandle Interface is implemented */
-    if ($event instanceof ShouldHandle) {
+    public function dispatch($event, $payload = [], $halt = false)
+    {
+        /* Handle the event Async when the ShouldHandle Interface is implemented */
+        if ($event instanceof ShouldHandle) {
 
-      /* Initialize delay & queue variables */
-      $delay = $event->jobDelay;
-      $queue = $event->jobQueue;
+            /* Initialize delay & queue variables */
+            $delay = $event->jobDelay;
+            $queue = $event->jobQueue;
 
-      /* Create a job & initialize the dispatcher */
-      $job = new EventJob($event);
-      $dispatcher = new JobDispatcher($job);
+            /* Create a job & initialize the dispatcher */
+            $job = new EventJob($event);
+            $dispatcher = (new JobDispatcher($job));
 
-      if (isset($delay) && (is_numeric($delay) ||
-          $delay instanceof \DateTimeInterface ||
-          $delay instanceof \DateInterval
-        )) {
-        $dispatcher->delay($delay);
-      }
+            /* Check if the delay is set and if it has the correct type */
+            if (isset($delay)
+                && (is_numeric($delay)
+                    || $delay instanceof \DateTimeInterface
+                    || $delay instanceof \DateInterval
+                )
+            ) {
+                $dispatcher->delay($delay);
+            }
+            /* Check if the queue is set and if it is a string */
+            if (isset($queue) && is_string($queue)) {
+                $dispatcher->onQueue($queue);
+            }
 
-      if (isset($queue) && is_string($queue)) {
-        $dispatcher->onQueue($queue);
-      }
+        } else {
+            if ($event instanceof ShouldHandleNow) {
+                $event->handle();
+            }
+        }
 
-    } else if ($event instanceof ShouldHandleNow) {
-      $event->handle();
+        return parent::dispatch($event, $payload, $halt);
     }
-
-    return parent::dispatch($event, $payload, $halt);
-  }
 }
