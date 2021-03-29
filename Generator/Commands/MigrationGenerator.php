37defc755e157c087d849f3ebd77408f0b2dfd4d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 09:51:54 +0430

    now migrations are generated with correct name

--- Generator/Commands/MigrationGenerator.php
+++ Generator/Commands/MigrationGenerator.php
@@ -6,53 +6,12 @@ use Apiato\Core\Generator\GeneratorCommand;
 use Apiato\Core\Generator\Interfaces\ComponentsGenerator;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Pluralizer;
 use Illuminate\Support\Str;
 use Symfony\Component\Console\Input\InputOption;
 
 class MigrationGenerator extends GeneratorCommand implements ComponentsGenerator
 {
-
-    /**
-     * The console command name.
-     *
-     * @var string
-     */
-    protected $name = 'apiato:generate:migration';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Create an "empty" migration file for a Container';
-
-    /**
-     * The type of class being generated.
-     *
-     * @var string
-     */
-    protected $fileType = 'Migration';
-
-    /**
-     * The structure of the file path.
-     *
-     * @var  string
-     */
-    protected $pathStructure = '{container-name}/Data/Migrations/*';
-
-    /**
-     * The structure of the file name.
-     *
-     * @var  string
-     */
-    protected $nameStructure = '{date}_{file-name}';
-
-    /**
-     * The name of the stub file.
-     *
-     * @var  string
-     */
-    protected $stubName = 'migration.stub';
 
     /**
      * User required/optional inputs expected to be passed while calling the command.
@@ -63,13 +22,49 @@ class MigrationGenerator extends GeneratorCommand implements ComponentsGenerator
     public $inputs = [
         ['tablename', null, InputOption::VALUE_OPTIONAL, 'The name for the database table'],
     ];
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'apiato:generate:migration';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an "empty" migration file for a Container';
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $fileType = 'Migration';
+    /**
+     * The structure of the file path.
+     *
+     * @var  string
+     */
+    protected $pathStructure = '{container-name}/Data/Migrations/*';
+    /**
+     * The structure of the file name.
+     *
+     * @var  string
+     */
+    protected $nameStructure = '{date}_{file-name}';
+    /**
+     * The name of the stub file.
+     *
+     * @var  string
+     */
+    protected $stubName = 'migration.stub';
 
     /**
      * @return array|null
      */
     public function getUserInputs()
     {
-        $tableName = Str::lower($this->checkParameterOrAsk('tablename', 'Enter the name of the database table'));
+        $tableName = Str::lower($this->checkParameterOrAsk('tablename', 'Enter the name of the database table', Pluralizer::plural(Str::lower($this->containerName))));
 
         // Now we need to check if there already exists a "default migration file" for this container!
         // We therefore search for a file that is named "xxxx_xx_xx_xxxxxx_NAME"
@@ -116,7 +111,7 @@ class MigrationGenerator extends GeneratorCommand implements ComponentsGenerator
      */
     public function getDefaultFileName(): string
     {
-        return 'create_' . Str::lower($this->containerName) . '_tables';
+        return 'create_' . Pluralizer::plural(Str::lower($this->containerName)) . '_table';
     }
 
     /**
