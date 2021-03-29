dd6377a3b5ae15ba30f6e175b68f16c595a9b3d5
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 16:49:37 +0430

    Move some traits

--- Abstracts/Models/UserModel.php
+++ Abstracts/Models/UserModel.php
@@ -2,9 +2,17 @@
 
 namespace Apiato\Core\Abstracts\Models;
 
+use Apiato\Core\Traits\FactoryLocatorTrait;
+use Apiato\Core\Traits\HashIdTrait;
+use Apiato\Core\Traits\HasResourceKeyTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as LaravelAuthenticatableUser;
 
 abstract class UserModel extends LaravelAuthenticatableUser
 {
-
+    use HashIdTrait;
+    use HasResourceKeyTrait;
+    use HasFactory, FactoryLocatorTrait {
+        FactoryLocatorTrait::newFactory insteadof HasFactory;
+    }
 }
