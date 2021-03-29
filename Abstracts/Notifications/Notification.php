728febb9c21c5e78e3e27ffdce55191f72869335
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 16:50:34 +0430

    made notificate abstract

--- Abstracts/Notifications/Notification.php
+++ Abstracts/Notifications/Notification.php
@@ -5,7 +5,7 @@ namespace Apiato\Core\Abstracts\Notifications;
 use Illuminate\Notifications\Notification as LaravelNotification;
 use Illuminate\Support\Facades\Config;
 
-class Notification extends LaravelNotification
+abstract class Notification extends LaravelNotification
 {
     public function via($notifiable): array
     {
