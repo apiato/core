c6140b7bc6664982ebc41461458d2ae1f6f92f66
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-29 17:53:20 +0430

    Move hasRole trait usage from ship to core

--- Abstracts/Models/UserModel.php
+++ Abstracts/Models/UserModel.php
@@ -7,11 +7,13 @@ use Apiato\Core\Traits\HashIdTrait;
 use Apiato\Core\Traits\HasResourceKeyTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as LaravelAuthenticatableUser;
+use Spatie\Permission\Traits\HasRoles;
 
 abstract class UserModel extends LaravelAuthenticatableUser
 {
     use HashIdTrait;
     use HasResourceKeyTrait;
+    use HasRoles;
     use HasFactory, FactoryLocatorTrait {
         FactoryLocatorTrait::newFactory insteadof HasFactory;
     }
