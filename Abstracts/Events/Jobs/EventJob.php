45b0528ca37cc8880601b0c1119169301bfb5a43
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2021-03-30 10:59:04 +0430

    Remove duplicate trait declaration

--- Abstracts/Events/Jobs/EventJob.php
+++ Abstracts/Events/Jobs/EventJob.php
@@ -4,16 +4,10 @@ namespace Apiato\Core\Abstracts\Events\Jobs;
 
 use Apiato\Core\Abstracts\Events\Interfaces\ShouldHandle;
 use Apiato\Core\Abstracts\Jobs\Job;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class EventJob extends Job implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
     public ShouldHandle $handler;
 
     public function __construct(ShouldHandle $handler)
