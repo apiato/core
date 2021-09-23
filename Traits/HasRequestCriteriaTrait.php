604aabdf55023333cbfde2a5c0e16229b7448807
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-23 10:46:52 +0330

    enable "Search Query Parameter with hashId" - fix 383

--- Traits/HasRequestCriteriaTrait.php
+++ Traits/HasRequestCriteriaTrait.php
@@ -5,13 +5,20 @@ namespace Apiato\Core\Traits;
 use Apiato\Core\Abstracts\Repositories\Repository;
 use Apiato\Core\Exceptions\CoreInternalErrorException;
 use Prettus\Repository\Criteria\RequestCriteria;
+use Prettus\Repository\Exceptions\RepositoryException;
+use Vinkla\Hashids\Facades\Hashids;
 
 trait HasRequestCriteriaTrait
 {
-    public function addRequestCriteria($repository = null): self
+    /**
+     * @throws CoreInternalErrorException
+     * @throws RepositoryException
+     */
+    public function addRequestCriteria($repository = null, array $fieldsToDecode = ['id']): self
     {
         $validatedRepository = $this->validateRepository($repository);
         $validatedRepository->pushCriteria(app(RequestCriteria::class));
+        $this->decodeSearchQueryString($fieldsToDecode);
         return $this;
     }
 
@@ -20,10 +27,11 @@ trait HasRequestCriteriaTrait
      *
      * @param $repository
      *
-     * @return mixed
+     * @return Repository
+     *
      * @throws CoreInternalErrorException
      */
-    private function validateRepository($repository)
+    private function validateRepository($repository): Repository
     {
         $validatedRepository = $repository;
 
@@ -48,6 +56,76 @@ trait HasRequestCriteriaTrait
         return $validatedRepository;
     }
 
+    private function decodeSearchQueryString(array $fieldsToDecode): void
+    {
+        if (!$this->hashIdEnabled()) {
+            return;
+        }
+
+        $query = request()->query();
+
+        if (array_key_exists('search', $query)) {
+            $query['search'] = $this->decodeSearchFields($fieldsToDecode, $query['search']);
+
+            request()->query->replace($query);
+        }
+    }
+
+    private function hashIdEnabled(): bool
+    {
+        return config('apiato.hash-id');
+    }
+
+    private function decodeSearchFields(array $fieldsToDecode, string $searchQuery): string
+    {
+        $searchArray = $this->searchQueryToArray($searchQuery);
+        foreach ($fieldsToDecode as $field) {
+            if (array_key_exists($field, $searchArray)) {
+                $searchArray[$field] = Hashids::decode($searchArray[$field])[0];
+            }
+        }
+        return $this->arrayToSearchQuery($searchArray);
+    }
+
+    private function searchQueryToArray($search): array
+    {
+        $searchArray = [];
+
+        $segments = $this->getSearchSegments($search);
+        foreach ($segments as $segment) {
+            try {
+                [$key, $value] = $this->getSearchSegmentKeyValues($segment);
+                $searchArray[$key] = $value;
+            } catch (\Exception $e) {
+                //Surround offset error
+            }
+        }
+
+        return $searchArray;
+    }
+
+    private function getSearchSegments($search): array
+    {
+        return explode(';', $search);
+    }
+
+    private function getSearchSegmentKeyValues($segment): array
+    {
+        return explode(':', $segment);
+    }
+
+    private function arrayToSearchQuery(array $decodedSearchArray): string
+    {
+        $decodedSearchQuery = '';
+        foreach ($decodedSearchArray as $key => $value) {
+            $decodedSearchQuery .= "$key:$value;";
+        }
+        return $decodedSearchQuery;
+    }
+
+    /**
+     * @throws CoreInternalErrorException
+     */
     public function removeRequestCriteria($repository = null): self
     {
         $validatedRepository = $this->validateRepository($repository);
