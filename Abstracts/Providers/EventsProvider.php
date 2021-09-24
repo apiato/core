c55fe7d6907a7337378c4723879d933d53cf20ab
mohammad-alavi <mohammad.alavi1990@gmail.com>
2021-09-24 18:46:40 +0330

    remove unneed parent boot call

--- Abstracts/Providers/EventsProvider.php
+++ Abstracts/Providers/EventsProvider.php
@@ -6,11 +6,5 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as LaravelEvent
 
 abstract class EventsProvider extends LaravelEventServiceProvider
 {
-    /**
-     * Register any other events for your application.
-     */
-    public function boot(): void
-    {
-        parent::boot();
-    }
+
 }
