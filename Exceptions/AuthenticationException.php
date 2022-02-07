92c4a6432b51be24268e54e8d95e971038be9cb8
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-02-07 19:51:25 +0330

    fix grammar

--- Exceptions/AuthenticationException.php
+++ Exceptions/AuthenticationException.php
@@ -8,5 +8,5 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthenticationException extends Exception
 {
     protected $code = Response::HTTP_UNAUTHORIZED;
-    protected $message = 'An Exception occurred when trying to authenticate the User.';
+    protected $message = 'An Exception occurred while trying to authenticate the User.';
 }
