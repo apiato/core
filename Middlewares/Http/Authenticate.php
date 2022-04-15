8ce874ed455601418e5d29b4a1da378427564b44
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-04-15 11:23:09 +0430

    feat!: remove function override (which was not working anyway!)

--- Middlewares/Http/Authenticate.php
+++ Middlewares/Http/Authenticate.php
@@ -2,27 +2,9 @@
 
 namespace Apiato\Core\Middlewares\Http;
 
-use Apiato\Core\Exceptions\AuthenticationException;
-use Exception;
-use Illuminate\Auth\AuthenticationException as LaravelAuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * @throws LaravelAuthenticationException
-     * @throws AuthenticationException
-     */
-    protected function authenticate($request, array $guards): void
-    {
-        try {
-            parent::authenticate($request, $guards);
-        } catch (Exception) {
-            if ($request->expectsJson()) {
-                throw new AuthenticationException();
-            } else {
-                $this->unauthenticated($request, $guards);
-            }
-        }
-    }
+
 }
