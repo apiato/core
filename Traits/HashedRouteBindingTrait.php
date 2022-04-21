8a4d59339b18f738e2b7e7dd2d6e76c1cbc93a6c
Mohammad Alavi <mohammad.alavi1990@gmail.com>
2022-04-21 16:29:19 +0430

    feat: resolve child route bindings

--- Traits/HashedRouteBindingTrait.php
+++ Traits/HashedRouteBindingTrait.php
@@ -4,10 +4,34 @@ namespace Apiato\Core\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Str;
 use Vinkla\Hashids\Facades\Hashids;
 
 trait HashedRouteBindingTrait
 {
+    /**
+     * Retrieve the child model query for a bound value.
+     *
+     * @param string $childType
+     * @param mixed $value
+     * @param string|null $field
+     * @return Relation
+     */
+    protected function resolveChildRouteBindingQuery($childType, $value, $field)
+    {
+        if (method_exists($this, Str::plural(Str::camel($childType)))) {
+            return parent::resolveChildRouteBindingQuery($childType, $value, $field);
+        }
+
+        $relationship = $this->{Str::camel($childType)}();
+
+        $field = $field ?: $relationship->getRelated()->getRouteKeyName();
+
+        return $relationship instanceof Model
+            ? $relationship->resolveRouteBindingQuery($relationship, $value, $field)
+            : $relationship->getRelated()->resolveRouteBindingQuery($relationship, $value, $field);
+    }
+
     /**
      * Retrieve the model for a bound value.
      *
