2add1216cd5aa786c3e7c413af7464bbe9d071ae
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2022-12-03 11:58:12 +0330

    Revert "Merge branch 'master' into 8.x"
    This reverts commit 6d83ad2df4de3e36095d68fd7980fb6a679f189e, reversing
    changes made to 15bcd89c95b37b46ede6991544b398baf66327e6.

--- Traits/HashIdTrait.php
+++ Traits/HashIdTrait.php
@@ -56,29 +56,15 @@ trait HashIdTrait
         return $result;
     }
 
-    /**
-     * @param string|null $id
-     * @return int|null
-     *
-     * if the decoded id is bigger than PHP_INT_MAX, the decoder will return a string
-     * we will cut that off from propagating, because such big numerical identifiers
-     * are not practically used
-     *
-     * if the id is not decodable, null will be returned
-     */
-    public function decode(?string $id): ?int
+    public function decode($id)
     {
         // check if passed as null, (could be an optional decodable variable)
-        if (is_null($id) || strtolower($id) === 'null') {
+        if (is_null($id) || strtolower($id) == 'null') {
             return $id;
         }
 
         // do the decoding if the ID looks like a hashed one
-        if (!empty($this->decoder($id))) {
-            return is_string($this->decoder($id)[0]) ? null : $this->decoder($id)[0];
-        }
-
-        return null;
+        return empty($this->decoder($id)) ? [] : $this->decoder($id)[0];
     }
 
     private function decoder($id): array
