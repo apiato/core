c97440290aa009479e63ad082a4d89a5626066d9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-04 17:53:11 +0330

    feat! remove import, sync with Laravel

--- Abstracts/Controllers/Controller.php
+++ Abstracts/Controllers/Controller.php
@@ -4,14 +4,12 @@ namespace Apiato\Core\Abstracts\Controllers;
 
 use Apiato\Core\Traits\HashIdTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as LaravelBaseController;
 
 abstract class Controller extends LaravelBaseController
 {
     use AuthorizesRequests;
-    use DispatchesJobs;
     use ValidatesRequests;
     use HashIdTrait;
 }
