d4cba1408f415cabc74032cb9b2ea550d7bd60c1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-06-04 18:01:57 +0330

    fix: prevent passing null to explode

--- Traits/ResponseTrait.php
+++ Traits/ResponseTrait.php
@@ -87,7 +87,7 @@ trait ResponseTrait
 
     protected function parseRequestedIncludes(): array
     {
-        return explode(',', Request::get('include'));
+        return explode(',', Request::get('include') ?? '');
     }
 
     private function filterResponse(array $responseArray, array $filters): array
