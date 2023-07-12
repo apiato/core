3880aa27fec3e3bb4c24d9c526fcd56c4153f557
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-07-12 21:09:26 +0330

    feat: add `assertDatabaseTable` test assertion method

--- Traits/TestTraits/PhpUnit/TestAssertionHelperTrait.php
+++ Traits/TestTraits/PhpUnit/TestAssertionHelperTrait.php
@@ -5,6 +5,7 @@ namespace Apiato\Core\Traits\TestTraits\PhpUnit;
 use Apiato\Core\Abstracts\Models\Model;
 use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Schema;
 use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\MockObject\MockObject;
 
@@ -56,5 +57,22 @@ trait TestAssertionHelperTrait
     protected function inIds($id, Collection $collection): bool
     {
         return in_array($id, $collection->map(fn ($item) => $item->getHashedKey())->toArray());
+    }
+
+    /**
+     * Assert if the given database table has the expected columns with the expected types.
+     *
+     * @param string $table The table name.
+     * @param array<string, string> $expectedColumns The key is the column name and the value is the column type.
+     *
+     * Example: $this->assertDatabaseTable('users', ['id' => 'bigint']);
+     */
+    protected function assertDatabaseTable(string $table, array $expectedColumns): void
+    {
+        $this->assertSameSize($expectedColumns, Schema::getColumnListing($table), "Column count mismatch for '$table' table.");
+        foreach ($expectedColumns as $column => $type) {
+            $this->assertTrue(Schema::hasColumn($table, $column), "Column '$column' not found in '$table' table.");
+            $this->assertEquals($type, Schema::getColumnType($table, $column), "Column '$column' in '$table' table does not match expected $type type.");
+        }
     }
 }
