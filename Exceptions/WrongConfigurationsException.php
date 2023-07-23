68304b8b413b8e386d2bbc0dbdaa3036bd74bb2b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-07-23 15:48:57 +0330

    feat: marked as deprecated

--- Exceptions/WrongConfigurationsException.php
+++ Exceptions/WrongConfigurationsException.php
@@ -3,8 +3,12 @@
 namespace Apiato\Core\Exceptions;
 
 use Apiato\Core\Abstracts\Exceptions\Exception;
+use JetBrains\PhpStorm\Deprecated;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Deprecated(
+    reason: 'This exception is not used anywhere and will be removed in the next major release.'
+)]
 class WrongConfigurationsException extends Exception
 {
     protected $code = Response::HTTP_INTERNAL_SERVER_ERROR;
