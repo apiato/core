2af6e441efec9511e44c3203b4fafbd3b01bdd08
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2023-12-16 05:21:04 +0330

    docs: add phpdoc

--- Abstracts/Factories/Factory.php
+++ Abstracts/Factories/Factory.php
@@ -3,7 +3,13 @@
 namespace Apiato\Core\Abstracts\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory as LaravelFactory;
+use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @template TModel of Model
+ *
+ * @extends LaravelFactory<TModel>
+ */
 abstract class Factory extends LaravelFactory
 {
 }
