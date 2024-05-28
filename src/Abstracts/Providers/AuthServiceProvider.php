df9973240d13ec2ea933bf08235f97ed6ecf9dc3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-05-28 20:26:18 +0330

    feat: don't manually register policies
    https://laravel.com/docs/10.x/upgrade#register-policies

--- src/Abstracts/Providers/AuthServiceProvider.php
+++ src/Abstracts/Providers/AuthServiceProvider.php
@@ -6,20 +6,4 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as LaravelAuthSe
 
 abstract class AuthServiceProvider extends LaravelAuthServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        $this->registerPolicies();
-    }
 }
