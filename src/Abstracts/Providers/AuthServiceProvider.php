b69114b37c1708068808a7d0109783976138f904
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-05-28 21:16:31 +0330

    Revert "feat: don't manually register policies"
    This reverts commit df9973240d13ec2ea933bf08235f97ed6ecf9dc3.

--- src/Abstracts/Providers/AuthServiceProvider.php
+++ src/Abstracts/Providers/AuthServiceProvider.php
@@ -6,4 +6,20 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as LaravelAuthSe
 
 abstract class AuthServiceProvider extends LaravelAuthServiceProvider
 {
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
 }
