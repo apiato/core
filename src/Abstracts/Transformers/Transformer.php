6068aefad60a3a48efdca0d9fb5a66a111abc0d3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-06-03 11:17:13 +0330

    feat: add default resource key if no key present

--- src/Abstracts/Transformers/Transformer.php
+++ src/Abstracts/Transformers/Transformer.php
@@ -2,6 +2,8 @@
 
 namespace Apiato\Core\Abstracts\Transformers;
 
+use Apiato\Core\Contracts\HasResourceKey;
+use League\Fractal\Resource\Collection;
 use League\Fractal\Resource\Item;
 use League\Fractal\Resource\Primitive;
 use League\Fractal\TransformerAbstract as FractalTransformer;
@@ -15,5 +17,26 @@ abstract class Transformer extends FractalTransformer
         }
 
         return $this->item($data, $transformer, $resourceKey);
+    }
+
+    protected function item($data, $transformer, string|null $resourceKey = null): Item
+    {
+        if (!$resourceKey && $data instanceof HasResourceKey) {
+            $resourceKey = $data->getResourceKey();
+        }
+
+        return parent::item($data, $transformer, $resourceKey);
+    }
+
+    protected function collection($data, $transformer, string|null $resourceKey = null): Collection
+    {
+        if (!$resourceKey && $data->isNotEmpty()) {
+            $firstItem = $data->first();
+            if ($firstItem instanceof HasResourceKey) {
+                $resourceKey = $firstItem->getResourceKey();
+            }
+        }
+
+        return parent::collection($data, $transformer, $resourceKey);
     }
 }
