7df3863880a45b8fcab02b57e1de0b70da7cc10a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-06-03 18:56:06 +0330

    test(unit): add repository test

---
+++ tests/Infrastructure/Doubles/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Apiato\Core\Tests\Infrastructure\Doubles;
+
+use Apiato\Core\Abstracts\Repositories\Repository as ParentRepository;
+
+class UserRepository extends ParentRepository
+{
+    protected $fieldSearchable = [
+        'name' => 'ilike',
+    ];
+
+    public function model(): string
+    {
+        return User::class;
+    }
+}
