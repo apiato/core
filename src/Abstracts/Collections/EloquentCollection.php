3774b42edf5767c2ed2a7f5559fb558112877fd3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-06-08 16:29:14 +0330

    remove: duplicate method which is already added via macros

--- src/Abstracts/Collections/EloquentCollection.php
+++ src/Abstracts/Collections/EloquentCollection.php
@@ -3,15 +3,7 @@
 namespace Apiato\Core\Abstracts\Collections;
 
 use Illuminate\Database\Eloquent\Collection;
-use Vinkla\Hashids\Facades\Hashids;
 
 abstract class EloquentCollection extends Collection
 {
-    /**
-     * Check if the given hashed id exists in the collection
-     */
-    public function containsHashedId(string $hashedId, string $key = 'id'): bool
-    {
-        return $this->contains($key, Hashids::decode($hashedId)[0]);
-    }
 }
