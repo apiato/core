d2c591a57b995bccd6d73def64b7388aaa278a54
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-06-09 20:28:53 +0330

    test: 'override resource name' functionality

--- tests/Unit/Services/ResponseTest.php
+++ tests/Unit/Services/ResponseTest.php
@@ -141,28 +141,38 @@ class ResponseTest extends UnitTestCase
             'without includes' => [
                 'fieldset' => ['User:id;email'],
                 'expected' => ['data.id', 'data.email'],
+                'missing' => ['data.object', 'data.name', 'data.created_at', 'data.updated_at', 'data.children', 'data.books']
+            ],
+            'only filter nested include keys' => [
+                'fieldset' => ['Book:author;title'],
+                'expected' => ['data.object', 'data.id', 'data.email', 'data.name', 'data.created_at', 'data.updated_at', 'data.books.data.0.author', 'data.books.data.0.title'],
+                'missing' => ['data.books.data.0.id', 'data.books.data.0.created_at', 'data.books.data.0.updated_at']
             ],
             'with first level includes - no filter' => [
                 'fieldset' => ['User:object,id;email;books'],
                 'expected' => ['data.object', 'data.id', 'data.email', 'data.books.data.0.object', 'data.books.data.0.id', 'data.books.data.0.title', 'data.books.data.0.author', 'data.books.data.0.created_at', 'data.books.data.0.updated_at'],
+                'missing' => ['data.name', 'data.created_at', 'data.updated_at']
             ],
             'with first level includes - filter' => [
                 'fieldset' => ['User:object,id;email;books', 'Book:object,author'],
                 'expected' => ['data.object', 'data.id', 'data.email', 'data.books.data.0.object', 'data.books.data.0.author'],
+                'missing' => ['data.children', 'data.books.data.0.id', 'data.books.data.0.title', 'data.books.data.0.created_at', 'data.books.data.0.updated_at', 'data.name', 'data.created_at', 'data.updated_at']
             ],
             'with nested includes - no filter' => [
                 'fieldset' => ['User:object,id;email,children;books'],
                 'expected' => ['data.object', 'data.id', 'data.email', 'data.children.data.0.object', 'data.children.data.0.id', 'data.children.data.0.email', 'data.children.data.0.books.data.0.object', 'data.children.data.0.books.data.0.id', 'data.children.data.0.books.data.0.title', 'data.children.data.0.books.data.0.author', 'data.children.data.0.books.data.0.created_at', 'data.children.data.0.books.data.0.updated_at'],
+                'missing' => ['data.name', 'data.created_at', 'data.updated_at']
             ],
             'with nested includes - filter' => [
-                'fieldset' => ['User:object,id;email;children;books', 'Books:id'],
-                'expected' => ['data.object', 'data.id', 'data.email', 'data.children.data.0.object', 'data.children.data.0.id', 'data.children.data.0.email', 'data.children.data.0.books.data.0.id'],
+                'fieldset' => ['User:id;email;children;books', 'Book:id'],
+                'expected' => ['data.id', 'data.email', 'data.children.data.0.id', 'data.children.data.0.email', 'data.children.data.0.books.data.0.id'],
+                'missing' => ['data.object', 'data.children.data.0.object', 'data.children.data.0.books.data.0.object', 'data.children.data.0.books.data.0.title', 'data.children.data.0.books.data.0.author', 'data.children.data.0.books.data.0.created_at', 'data.children.data.0.books.data.0.updated_at', 'data.name', 'data.created_at', 'data.updated_at']
             ],
         ];
     }
 
     #[DataProvider('fieldsetDataProvider')]
-    public function testCanFilterResponse($fieldset, $expected): void
+    public function testCanFilterResponse($fieldset, $expected, $missing): void
     {
         request()->merge(['include' => 'books,children.books', 'fieldset' => $fieldset]);
         $response = Response::createFrom($this->user);
@@ -173,6 +183,9 @@ class ResponseTest extends UnitTestCase
         foreach ($expected as $expectation) {
             $result->has($expectation);
             $result->has('meta.include', fn (AssertableJson $json) => $json->whereAll(['parent', 'children', 'books']));
+        }
+        foreach ($missing as $expectation) {
+            $result->missing($expectation);
         }
     }
 
@@ -282,5 +295,55 @@ class ResponseTest extends UnitTestCase
         $result = AssertableJson::fromArray($response->toArray());
 
         $result->has('meta.include', fn (AssertableJson $json) => $json->whereAll(['parent', 'children', 'books']));
+    }
+
+    public static function validResourceNameProvider(): array
+    {
+        return [
+            'empty string' => [
+                'resourceName' => '',
+            ],
+            'string' => [
+                'resourceName' => 'wat',
+            ],
+        ];
+    }
+
+    #[DataProvider('validResourceNameProvider')]
+    public function testCanOverrideMainResourceName($resourceName): void
+    {
+        request()->merge(['include' => 'books,children.books', 'fieldset' => ["{$resourceName}:id", 'Book:author,title']]);
+        $response = Response::createFrom($this->user);
+        $response->transformWith(UserTransformer::class);
+        $response->withResourceName($resourceName);
+
+        $result = AssertableJson::fromArray($response->toArray());
+
+        $result->missing('data.object');
+    }
+
+    public static function invalidResourceNameProvider(): array
+    {
+        return [
+            'null' => [
+                'resourceName' => null,
+            ],
+            'false' => [
+                'resourceName' => false,
+            ],
+        ];
+    }
+
+    #[DataProvider('invalidResourceNameProvider')]
+    public function testGivenInvalidNameProvidedRevertToDefaultMainResourceName($resourceName): void
+    {
+        request()->merge(['include' => 'books,children.books', 'fieldset' => ["{$resourceName}:id", 'Book:author,title']]);
+        $response = Response::createFrom($this->user);
+        $response->transformWith(UserTransformer::class);
+        $response->withResourceName($resourceName);
+
+        $result = AssertableJson::fromArray($response->toArray());
+
+        $result->has('data.object');
     }
 }
