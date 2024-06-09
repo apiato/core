c954f0c14a6af95c6d91d4b6e7527efb8a56c950
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-06-09 19:02:38 +0330

    test: add test models and relations

--- tests/Infrastructure/Doubles/UserTransformer.php
+++ tests/Infrastructure/Doubles/UserTransformer.php
@@ -5,16 +5,17 @@ namespace Apiato\Core\Tests\Infrastructure\Doubles;
 use Apiato\Core\Abstracts\Transformers\Transformer;
 use League\Fractal\Resource\Collection;
 use League\Fractal\Resource\Item;
+use League\Fractal\Resource\Primitive;
 
 class UserTransformer extends Transformer
 {
     protected array $availableIncludes = [
         'parent',
+        'children',
+        'books',
     ];
 
-    protected array $defaultIncludes = [
-        'children',
-    ];
+    protected array $defaultIncludes = [];
 
     public function transform(User $user): array
     {
@@ -28,13 +29,18 @@ class UserTransformer extends Transformer
         ];
     }
 
-    public function includeParent(User $user): Item
+    public function includeParent(User $user): Primitive|Item
     {
-        return $this->item($user->parent, new static());
+        return $this->nullableItem($user->parent, new static());
     }
 
     public function includeChildren(User $user): Collection
     {
         return $this->collection($user->children, new static());
+    }
+
+    public function includeBooks(User $user): Collection
+    {
+        return $this->collection($user->books, new BookTransformer());
     }
 }
