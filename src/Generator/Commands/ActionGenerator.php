2d58bc0760236805e6704e09c1c55ed29d2c8bd3
Moslem Deris <moslem.deris@gmail.com>
2024-08-19 20:30:10 +0330

    Prepare an example of how to work with `php-generator` package XL-9040

--- src/Generator/Commands/ActionGenerator.php
+++ src/Generator/Commands/ActionGenerator.php
@@ -76,20 +76,100 @@ class ActionGenerator extends GeneratorCommand
 
     protected function getStubFileName(): string
     {
-        return "actions/{$this->stub}.stub";
+        $file = new \Nette\PhpGenerator\PhpFile;
+        $namespace = $file->addNamespace('App\Containers\\' . $this->sectionName . '\\' . $this->containerName . '\Actions');
+        $isCRUD = in_array($this->stub, ['list', 'find', 'create', 'update', 'delete']);
+
+        // imports
+        $parentActionFullPath = 'App\Ship\Parents\Actions\Action';
+        $namespace->addUse($parentActionFullPath, 'ParentAction');
+        if ($isCRUD) {
+            if (in_array($this->stub, ['create', 'update', 'find'])) {
+                $modelFullPath = 'App\Containers\\' . $this->sectionName . '\\' . $this->containerName . '\Models\\' . $this->model;
+                $namespace->addUse($modelFullPath);
+            }elseif ($this->stub === 'list') {
+                $lengthAwarePaginatorFullPath = '\Illuminate\Contracts\Pagination\LengthAwarePaginator';
+                $namespace->addUse($lengthAwarePaginatorFullPath);
+            }
+            $taskFullPath = 'App\Containers\\' . $this->sectionName . '\\' . $this->containerName . '\Tasks\\' . Str::ucfirst($this->stub) . $this->model . ($this->stub === 'list' ? 's' : '') . 'Task';
+            $namespace->addUse($taskFullPath);
+            $requestFullPath = 'App\Containers\\' . $this->sectionName . '\\' . $this->containerName . '\UI\\' . $this->ui . '\Requests\\' . Str::ucfirst($this->stub) . $this->model . ($this->stub === 'list' ? 's' : '') . 'Request';
+            $namespace->addUse($requestFullPath);
+        }
+
+        // class
+        $class = $file->addNamespace($namespace)
+            ->addClass($this->fileName)
+            ->setExtends($parentActionFullPath);
+
+        // constructor
+        $constructor = $class->addMethod('__construct');
+        if ($isCRUD) {
+            $constructor->addPromotedParameter(Str::lower($this->stub) . $this->model . ($this->stub === 'list' ? 's' : '') . 'Task')
+                ->setPrivate()
+                ->setReadOnly()
+                ->setType($taskFullPath);
+        }
+
+        // run method
+        $runMethod = $class->addMethod('run')->setPublic();
+        if ($isCRUD) {
+            $runMethod->addPromotedParameter('request')
+                ->setPrivate()
+                ->setType($requestFullPath);
+
+            if ($this->stub === 'create') {
+                $runMethod->setReturnType($modelFullPath);
+
+                $runMethod->addBody("\$data = \$request->sanitizeInput([
+    // add your request data here
+]);
+            ");
+                $runMethod->addBody("return \$this->?{$this->model}Task->run(\$data);", [Str::lower($this->stub)]);
+            }elseif ($this->stub === 'update') {
+                $runMethod->setReturnType($modelFullPath);
+
+                $runMethod->addBody("\$data = \$request->sanitizeInput([
+    // add your request data here
+]);
+            ");
+                $runMethod->addBody("return \$this->?{$this->model}Task->run(\$data, \$request->id);", [Str::lower($this->stub)]);
+
+            }elseif ($this->stub === 'delete') {
+                $runMethod->setReturnType('int');
+
+                $runMethod->addBody("return \$this->?{$this->model}Task->run(\$request->id);", [Str::lower($this->stub)]);
+            }elseif ($this->stub === 'find') {
+                $runMethod->setReturnType($modelFullPath);
+
+                $runMethod->addBody("return \$this->?{$this->model}Task->run(\$request->id);", [Str::lower($this->stub)]);
+            }elseif ($this->stub === 'list') {
+                $runMethod->setReturnType($lengthAwarePaginatorFullPath);
+
+                $runMethod->addBody("return \$this->?Task->run();", [Str::lower($this->stub) . Str::plural($this->model)]);
+            }
+        } else {
+            $runMethod->setReturnType('void');
+        }
+
+        // return the file
+        return $file;
+
+// or use the PsrPrinter for output in accordance with PSR-2 / PSR-12 / PER
+// echo (new Nette\PhpGenerator\PsrPrinter)->printFile($file);
     }
 
     protected function getStubParameters(): array
     {
         return [
-            '_section-name' => Str::lower($this->sectionName),
-            'section-name' => $this->sectionName,
-            '_container-name' => Str::lower($this->containerName),
-            'container-name' => $this->containerName,
-            'class-name' => $this->fileName,
-            'model' => $this->model,
-            'models' => Pluralizer::plural($this->model),
-            'ui' => $this->ui,
+//            '_section-name' => Str::lower($this->sectionName),
+//            'section-name' => $this->sectionName,
+//            '_container-name' => Str::lower($this->containerName),
+//            'container-name' => $this->containerName,
+//            'class-name' => $this->fileName,
+//            'model' => $this->model,
+//            'models' => Pluralizer::plural($this->model),
+//            'ui' => $this->ui,
         ];
     }
 }
