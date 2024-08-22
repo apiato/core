d42d0f170c483e3f8b2c118593f525bdf2d121a7
Mohammad-Alavi <Mohammad-Alavi@users.noreply.github.com>
2024-08-22 15:52:14 +0000

    style: apply automated php-cs-fixer changes

--- src/Macros/Config/UnsetKey.php
+++ src/Macros/Config/UnsetKey.php
@@ -5,11 +5,12 @@ namespace Apiato\Core\Macros\Config;
 use Illuminate\Config\Repository;
 use Illuminate\Support\Arr;
 
-class UnsetKey {
+class UnsetKey
+{
     public function __invoke(): callable
     {
         return function (array|string|int|float $key): void {
-            /** @var Repository $this */
+            /* @var Repository $this */
             Arr::forget($this->items, $key);
         };
     }
