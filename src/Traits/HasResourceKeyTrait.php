0e5664195eeceeeeac6f685632cc35879cd9dbed
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-08-23 16:25:58 +0330

    docs: update docblock

--- src/Traits/HasResourceKeyTrait.php
+++ src/Traits/HasResourceKeyTrait.php
@@ -9,11 +9,14 @@ trait HasResourceKeyTrait
     /**
      * @var string|null
      */
-    #[Deprecated(reason: 'Override the getResourceKey method in the respective model class instead of using the $resourceKey property.')]
+    #[Deprecated(reason: 'Override the getResourceKey method in the respective model class instead of using the $resourceKey property')]
     protected $resourceKey;
 
     /**
-     * Returns the type for JSON API Serializer. Can be overwritten with the protected $resourceKey in respective model class.
+     * Returns the type for JSON API Serializer.
+     *
+     * If the $resourceKey property is set, it will be used as the resource key.
+     * Otherwise, the class name will be used.
      */
     public function getResourceKey(): string
     {
