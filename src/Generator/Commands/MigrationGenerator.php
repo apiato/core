b4172239f9606b99b2ad5711bb80759505b79575
Moslem Deris <moslem.deris@gmail.com>
2024-08-27 20:14:25 +0330

    add `HasTest` trait

--- src/Generator/Commands/MigrationGenerator.php
+++ src/Generator/Commands/MigrationGenerator.php
@@ -3,7 +3,9 @@
 namespace Apiato\Core\Generator\Commands;
 
 use Apiato\Core\Generator\FileGeneratorCommand;
+use Apiato\Core\Generator\Traits\HasTestTrait;
 
 class MigrationGenerator extends FileGeneratorCommand
 {
+    use HasTestTrait;
 }
