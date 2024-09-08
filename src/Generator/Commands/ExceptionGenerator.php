d304c561b3646740fb1c276a65f1d501a6e097b6
Moslem Deris <moslem.deris@gmail.com>
2024-09-08 20:42:42 +0330

    add logic to `Exception Generator` class

--- src/Generator/Commands/ExceptionGenerator.php
+++ src/Generator/Commands/ExceptionGenerator.php
@@ -3,7 +3,65 @@
 namespace Apiato\Core\Generator\Commands;
 
 use Apiato\Core\Generator\FileGeneratorCommand;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionGenerator extends FileGeneratorCommand
 {
+    public static function getCommandName(): string
+    {
+        return 'apiato:make:exception';
+    }
+
+    public static function getCommandDescription(): string
+    {
+        return 'Create an Exception for a Container';
+    }
+
+    public static function getFileType(): string
+    {
+        return 'exception';
+    }
+
+    protected static function getCustomCommandArguments(): array
+    {
+        return [
+        ];
+    }
+
+    protected function askCustomInputs(): void
+    {
+    }
+
+    protected function getFilePath(): string
+    {
+        return "$this->sectionName/$this->containerName/Exceptions/$this->fileName.php";
+    }
+
+    protected function getFileContent(): string
+    {
+        $file = new \Nette\PhpGenerator\PhpFile();
+        $namespace = $file->addNamespace('App\Containers\\' . $this->sectionName . '\\' . $this->containerName . '\Exceptions');
+
+        // imports
+        $parentExceptionFullPath = 'App\Ship\Parents\Exceptions\Exception';
+        $namespace->addUse($parentExceptionFullPath, 'ParentException');
+        $responseFullPath = 'Symfony\Component\HttpFoundation\Response';
+        $namespace->addUse($responseFullPath);
+
+        // class
+        $class = $file->addNamespace($namespace)
+            ->addClass($this->fileName)
+            ->setExtends($parentExceptionFullPath);
+
+        // properties
+        $class->addProperty('code')
+            ->setVisibility('protected')
+            ->setValue(Response::HTTP_BAD_REQUEST); //  TODO: find a way to initialize it with `Response::HTTP_BAD_REQUEST` not 400
+
+        $class->addProperty('message')
+            ->setVisibility('protected')
+            ->setValue('Exception Default Message.');
+
+        return $file;
+    }
 }
