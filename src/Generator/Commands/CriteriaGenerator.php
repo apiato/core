03ea69625cb227d22ad3f8f2774f0431da501d45
Moslem Deris <moslem.deris@gmail.com>
2024-09-11 05:45:18 +0330

    add `Criteria Generator` class

---
+++ src/Generator/Commands/CriteriaGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Apiato\Core\Generator\Commands;
+
+use Apiato\Core\Generator\FileGeneratorCommand;
+use Apiato\Core\Generator\Traits\HasTestTrait;
+
+class CriteriaGenerator extends FileGeneratorCommand
+{
+    use HasTestTrait;
+}
