9ece34dce4638d86347faf65378d7cd70ebef52d
Sebastian Weckend <sebastian.weckend@posteo.de>
2024-09-12 10:07:54 +0200

    chore: add function comments

--- src/Traits/TestTraits/PhpUnit/TestDatabaseProfilerTrait.php
+++ src/Traits/TestTraits/PhpUnit/TestDatabaseProfilerTrait.php
@@ -4,21 +4,33 @@ namespace Apiato\Core\Traits\TestTraits\PhpUnit;
 
 trait TestDatabaseProfilerTrait
 {
+    /**
+     * Start profiling database queries.
+     */
     protected function startDatabaseProfiler(): void
     {
         $this->app->make('db')->enableQueryLog();
     }
 
+    /**
+     * Stop profiling database queries.
+     */
     protected function stopDatabaseProfiler(): void
     {
         $this->app->make('db')->disableQueryLog();
     }
 
+    /**
+     * Get the database queries.
+     */
     protected function getDatabaseQueries(): array
     {
         return $this->app->make('db')->getQueryLog();
     }
 
+    /**
+     * Dump the database queries.
+     */
     protected function dumpDatabaseQueries(): void
     {
         foreach ($this->getDatabaseQueries() as $query) {
@@ -26,17 +38,26 @@ trait TestDatabaseProfilerTrait
         }
     }
 
+    /**
+     * Dump and die the database queries.
+     */
     protected function ddDatabaseQueries(): void
     {
         dd($this->getDatabaseQueries());
     }
 
+    /**
+     * Assert the number of database queries.
+     */
     protected function assertDatabaseQueriesCount(int $expectedCount): void
     {
         $actualCount = count($this->getDatabaseQueries());
         $this->assertEquals($expectedCount, $actualCount, "Expected $expectedCount database queries, but got $actualCount.");
     }
 
+    /**
+     * Wrapper to profile database queries.
+     */
     protected function profileDatabaseQueries(callable $callback): mixed
     {
         $this->startDatabaseProfiler();
@@ -46,6 +67,9 @@ trait TestDatabaseProfilerTrait
         return $result;
     }
 
+    /**
+     * Wrapper to profile database queries and assert the number of queries.
+     */
     protected function profileDatabaseQueriesCount(int $expectedCount, callable $callback): mixed
     {
         return $this->profileDatabaseQueries(function () use ($expectedCount, $callback) {
