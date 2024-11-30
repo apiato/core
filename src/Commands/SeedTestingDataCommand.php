7793618d990166ce999b97a86aa651f6733128de
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-11-30 18:18:36 +0330

    feat: create publishable default configs

--- src/Commands/SeedTestingDataCommand.php
+++ src/Commands/SeedTestingDataCommand.php
@@ -23,6 +23,16 @@ class SeedTestingDataCommand extends ConsoleCommand
 
     public function handle(): void
     {
+        if (!config('apiato.seeders.testing')) {
+            $this->error('No Testing Seeder Found, Please Check Your Config File.');
+            return;
+        }
+
+        if (!class_exists(config('apiato.seeders.testing'))) {
+            $this->error('Testing Seeder Class Not Found.');
+            return;
+        }
+
         $this->call('db:seed', [
             '--class' => config('apiato.seeders.testing'),
         ]);
