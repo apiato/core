9f5c3f52e12fd6dfd7ba2682e45ccaa9e8111b0f
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-28 17:38:44 +0330

    Create HttpException.php

---
+++ src/Abstracts/Exceptions/HttpException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Apiato\Core\Abstracts\Exceptions;
+
+use Symfony\Component\HttpKernel\Exception\HttpException as BaseHttpException;
+
+abstract class HttpException extends BaseHttpException
+{
+}
