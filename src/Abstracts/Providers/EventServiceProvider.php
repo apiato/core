c3eae06d56285c395174de01a081f13b733336c0
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-31 15:09:58 +0330

    Update EventServiceProvider.php

--- src/Abstracts/Providers/EventServiceProvider.php
+++ src/Abstracts/Providers/EventServiceProvider.php
@@ -2,18 +2,8 @@
 
 namespace Apiato\Core\Abstracts\Providers;
 
-use Apiato\Core\Foundation\Facades\Apiato;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as LaravelEventServiceProvider;
 
 abstract class EventServiceProvider extends LaravelEventServiceProvider
 {
-    public function shouldDiscoverEvents(): bool
-    {
-        return true;
-    }
-
-    protected function discoverEventsWithin(): array
-    {
-        return array_map(static fn (string $path) => $path . '/Listeners', Apiato::getAllContainerPaths());
-    }
 }
