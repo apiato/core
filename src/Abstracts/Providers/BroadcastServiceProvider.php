5a0ac04bc73b75b5bb036c278ca04deed1ad17c3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-31 15:09:16 +0330

    Delete BroadcastServiceProvider.php

--- src/Abstracts/Providers/BroadcastServiceProvider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Apiato\Core\Abstracts\Providers;
-
-use Illuminate\Support\ServiceProvider as LaravelBroadcastServiceProvider;
-
-abstract class BroadcastServiceProvider extends LaravelBroadcastServiceProvider
-{
-}
