12c7ddcc4964cb300a7e3cac46cc7f410c308ee9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-31 15:12:12 +0330

    Update CollectionMacroServiceProvider.php

--- src/Providers/MacroProviders/CollectionMacroServiceProvider.php
+++ src/Providers/MacroProviders/CollectionMacroServiceProvider.php
@@ -2,16 +2,14 @@
 
 namespace Apiato\Core\Providers\MacroProviders;
 
-use Apiato\Core\Abstracts\Providers\MainServiceProvider as AbstractMainServiceProvider;
+use Apiato\Core\Abstracts\Providers\AggregateServiceProvider;
 use Apiato\Core\Macros\Collection\ContainsDecodedHash;
 use Illuminate\Support\Collection;
 
-final class CollectionMacroServiceProvider extends AbstractMainServiceProvider
+final class CollectionMacroServiceProvider extends AggregateServiceProvider
 {
     public function boot(): void
     {
-        parent::boot();
-
         Collection::make($this->macros())
             ->reject(static fn ($class, $macro) => Collection::hasMacro($macro))
             ->each(static fn ($class, $macro) => Collection::macro($macro, app($class)()));
