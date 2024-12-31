ccae2aa82b3c628a1940fb9113b4d67ff6b8e8bf
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-12-31 15:00:18 +0330

    Update AuthServiceProvider.php

--- src/Abstracts/Providers/AuthServiceProvider.php
+++ src/Abstracts/Providers/AuthServiceProvider.php
@@ -6,5 +6,4 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as LaravelAuthSe
 
 abstract class AuthServiceProvider extends LaravelAuthServiceProvider
 {
-    protected $policies = [];
 }
