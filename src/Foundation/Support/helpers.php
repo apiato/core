975ecbfb8187eb4cf147cc4c9429ae744f474def
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-18 00:47:45 +0330

    feat: add shared path helper

--- src/Foundation/Support/helpers.php
+++ src/Foundation/Support/helpers.php
@@ -1,1 +1,13 @@
 <?php
+
+use Apiato\Foundation\Apiato;
+
+if (!function_exists('shared_path')) {
+    /**
+     * Get the path to the application's shared directory.
+     */
+    function shared_path(string $path = ''): string
+    {
+        return Apiato::instance()->sharedPath($path);
+    }
+}
