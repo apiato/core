09a16dc7a3e8dd69e85cd27d89b68ac0ff3c1b55
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-20 01:50:19 +0330

    Update FactoryDiscovery.php

--- src/Foundation/Support/Traits/Model/FactoryDiscovery.php
+++ src/Foundation/Support/Traits/Model/FactoryDiscovery.php
@@ -3,17 +3,17 @@
 namespace Apiato\Foundation\Support\Traits\Model;
 
 use Apiato\Abstract\Factories\Factory;
-use Apiato\Foundation\Apiato;
 
 trait FactoryDiscovery
 {
     protected static function newFactory(): Factory|null
     {
-        $factoryName = app()->make(Apiato::class)
+        $factoryName = apiato()
             ->factoryDiscovery()
             ->resolveFactoryName(static::class);
 
-        if ($factoryName) {
+        if (is_string($factoryName)) {
+            /** @var class-string<Factory> $factoryName */
             return $factoryName::new();
         }
 
