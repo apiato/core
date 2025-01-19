b1759b591b166552ae57e8cb4f249b0483964c37
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-19 22:07:48 +0330

    Update BookCreated.php

--- workbench/app/Containers/MySection/Book/Events/BookCreated.php
+++ workbench/app/Containers/MySection/Book/Events/BookCreated.php
@@ -2,8 +2,6 @@
 
 namespace Workbench\App\Containers\MySection\Book\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Workbench\App\Containers\MySection\Book\Models\Book;
 use Workbench\App\Ship\Parents\Events\Event as ParentEvent;
 
@@ -12,15 +10,5 @@ class BookCreated extends ParentEvent
     public function __construct(
         public readonly Book $book,
     ) {
-    }
-
-    /**
-     * @return Channel[]
-     */
-    public function broadcastOn(): array
-    {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
     }
 }
