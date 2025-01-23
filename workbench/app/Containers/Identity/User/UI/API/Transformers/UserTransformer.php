123fdeae3f1d77f74e086f6d24c9c9616a195158
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-24 02:50:01 +0330

    Update UserTransformer.php

--- workbench/app/Containers/Identity/User/UI/API/Transformers/UserTransformer.php
+++ workbench/app/Containers/Identity/User/UI/API/Transformers/UserTransformer.php
@@ -3,13 +3,13 @@
 namespace Workbench\App\Containers\Identity\User\UI\API\Transformers;
 
 use Apiato\Abstract\Transformers\Transformer;
-use League\Fractal\Resource\Collection;
-use League\Fractal\Resource\Item;
+use Apiato\Support\Resources\Collection;
+use Apiato\Support\Resources\Item;
 use League\Fractal\Resource\Primitive;
 use Workbench\App\Containers\Identity\User\Models\User;
 use Workbench\App\Containers\MySection\Book\UI\API\Transformers\BookTransformer;
 
-class UserTransformer extends Transformer
+final class UserTransformer extends Transformer
 {
     protected array $availableIncludes = [
         'parent',
@@ -33,12 +33,12 @@ class UserTransformer extends Transformer
 
     public function includeParent(User $user): Primitive|Item
     {
-        return $this->nullableItem($user->parent, new static());
+        return $this->nullableItem($user->parent, new self());
     }
 
     public function includeChildren(User $user): Collection
     {
-        return $this->collection($user->children, new static());
+        return $this->collection($user->children, new self());
     }
 
     public function includeBooks(User $user): Collection
