e6f818d8f384923a7bb17878f87a6bb0160ee700
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-25 13:56:25 +0330

    Update ValidateJsonContent.php

--- src/Support/Middleware/ValidateJsonContent.php
+++ src/Support/Middleware/ValidateJsonContent.php
@@ -4,10 +4,16 @@ namespace Apiato\Support\Middleware;
 
 use Apiato\Abstract\Middlewares\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class ValidateJsonContent extends Middleware
 {
-    public function handle(Request $request, \Closure $next)
+    /**
+     * @param Request $request
+     * @param \Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, \Closure $next): Response
     {
         if (!$request->expectsJson() && config('apiato.requests.force-accept-header')) {
             throw new \RuntimeException('Missing Accept Header: application/json');
