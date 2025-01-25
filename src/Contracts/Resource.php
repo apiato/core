c666d0fa07eaa91616a58b042f99411cf4bd04d7
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-25 23:03:18 +0330

    Update Resource.php

--- src/Contracts/Resource.php
+++ src/Contracts/Resource.php
@@ -5,7 +5,7 @@ namespace Apiato\Contracts;
 interface Resource
 {
     /**
-     * Get the resource key to be used for the JSON response.
+     * Get the resource key to be used for the response serialization.
      */
     public function getResourceKey(): string;
 }
