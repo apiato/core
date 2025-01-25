a041376e2d19763feff5521afdc74e7e3db1bcb9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-25 13:57:59 +0330

    Create ValidateJsonContentTest.php

---
+++ tests/Unit/Support/Middleware/ValidateJsonContentTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Apiato\Support\Middleware\ValidateJsonContent;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Request;
+
+describe(class_basename(ValidateJsonContent::class), function (): void {
+    beforeEach(function (): void {
+        config(['apiato.requests.use-etag' => true]);
+        $this->next = function (Illuminate\Http\Request $request): Response {
+            return response('content');
+        };
+    });
+
+    it('should throw an exception if the request does not expect JSON and feature is enabled', function (): void {
+        config(['apiato.requests.force-accept-header' => true]);
+        $request = Request::create('/test', 'GET');
+        $request->headers->remove('Accept');
+        $middleware = new ValidateJsonContent();
+
+        $this->expectException(RuntimeException::class);
+
+        $middleware->handle($request, $this->next);
+    });
+
+    it('should not throw an exception if the request expects JSON or feature is disabled', function (bool $enabled): void {
+        config(['apiato.requests.force-accept-header' => $enabled]);
+        $request = Request::create('/test', 'GET');
+        $request->headers->set('Accept', 'application/json');
+        $middleware = new ValidateJsonContent();
+
+        $response = $middleware->handle($request, $this->next);
+
+        expect($response)->toBeInstanceOf(Response::class);
+    })->with([
+        true,
+        false,
+    ]);
+})->covers(ValidateJsonContent::class);
