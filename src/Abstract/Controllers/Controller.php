6cbf200c690b57c8ea6560f22d298fda1cd730de
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-25 22:55:53 +0330

    feat: remove traits

--- src/Abstract/Controllers/Controller.php
+++ src/Abstract/Controllers/Controller.php
@@ -2,14 +2,6 @@
 
 namespace Apiato\Abstract\Controllers;
 
-use Apiato\Foundation\Support\Traits\HashId;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as LaravelBaseController;
-
-abstract class Controller extends LaravelBaseController
+abstract class Controller
 {
-    use AuthorizesRequests;
-    use ValidatesRequests;
-    use HashId;
 }
