e0570f43246df769d4fef999f39c4f76688c04f1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-26 22:45:33 +0330

    Update Action.php

--- src/Abstract/Actions/Action.php
+++ src/Abstract/Actions/Action.php
@@ -6,22 +6,8 @@ use Illuminate\Support\Facades\DB;
 
 abstract class Action
 {
-    protected string $ui;
-
     public function transactionalRun(...$arguments)
     {
         return DB::transaction(fn () => static::run(...$arguments));
-    }
-
-    public function getUI(): string
-    {
-        return $this->ui;
-    }
-
-    public function setUI(string $interface): static
-    {
-        $this->ui = $interface;
-
-        return $this;
     }
 }
