95e4f2f056cbe289dff4b6a118bfc87608835236
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-31 14:38:56 +0330

    Update Command.php

--- src/Abstract/Commands/Command.php
+++ src/Abstract/Commands/Command.php
@@ -6,9 +6,4 @@ use Illuminate\Console\Command as LaravelCommand;
 
 abstract class Command extends LaravelCommand
 {
-    /**
-     * The type of this controller. This will be accessibly mirrored in the Actions.
-     * Giving each Action the ability to modify it's internal business logic based on the UI type that called it.
-     */
-    public string $ui = 'cli';
 }
