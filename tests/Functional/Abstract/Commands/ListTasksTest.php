9f28b5a574c1226237a9ca310b91418bf5347e91
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-31 16:32:23 +0330

    Create ListTasksTest.php

---
+++ tests/Functional/Abstract/Commands/ListTasksTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Apiato\Commands\ListTasks;
+
+describe(class_basename(ListTasks::class), function (): void {
+    it('can list all actions', function (): void {
+        $this->artisan('apiato:list:tasks')
+            ->assertExitCode(0)
+            ->expectsOutput('[Book]')
+            ->expectsOutput(' - Create Book');
+    });
+
+    it('can list all actions with file name', function (): void {
+        $this->artisan('apiato:list:tasks', [
+            '--with-file-name' => true,
+        ])->assertExitCode(0)
+            ->expectsOutput('[Book]')
+            ->expectsOutput(' - Create Book (CreateBookTask.php)');
+    });
+})->covers(ListTasks::class);
