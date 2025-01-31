dcb5df4c6df6e7eecc8e7853e70a2d3ce247ee6d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-31 16:32:22 +0330

    Create ListActionsTest.php

---
+++ tests/Functional/Abstract/Commands/ListActionsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Apiato\Commands\ListActions;
+
+describe(class_basename(ListActions::class), function (): void {
+    it('can list all actions', function (): void {
+        $this->artisan('apiato:list:actions')
+            ->assertExitCode(0)
+            ->expectsOutput('[Author]')
+            ->expectsOutput(' - Simple')
+            ->expectsOutput('[Book]')
+            ->expectsOutput(' - Create Book');
+    });
+
+    it('can list all actions with file name', function (): void {
+        $this->artisan('apiato:list:actions', [
+            '--with-file-name' => true,
+        ])->assertExitCode(0)
+            ->expectsOutput('[Author]')
+            ->expectsOutput(' - Simple (SimpleAction.php)')
+            ->expectsOutput('[Book]')
+            ->expectsOutput(' - Create Book (CreateBookAction.php)');
+    });
+})->covers(ListActions::class);
