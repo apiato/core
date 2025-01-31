3af3541ea745aca45202be857c841232223afddf
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-01-31 14:38:54 +0330

    Update WebController.php

--- src/Abstract/Controllers/WebController.php
+++ src/Abstract/Controllers/WebController.php
@@ -4,9 +4,4 @@ namespace Apiato\Abstract\Controllers;
 
 abstract class WebController extends Controller
 {
-    /**
-     * The type of this controller. This will be accessibly mirrored in the Actions.
-     * Giving each Action the ability to modify it's internal business logic based on the UI type that called it.
-     */
-    public string $ui = 'web';
 }
