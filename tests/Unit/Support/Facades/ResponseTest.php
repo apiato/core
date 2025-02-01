03204e9981e232cbb0c0951dc6baec7f602c0978
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-01 16:11:56 +0330

    Create ResponseTest.php

---
+++ tests/Unit/Support/Facades/ResponseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use Apiato\Support\Facades\Response;
+
+describe(class_basename(Response::class), function (): void {
+    it('should have the correct facade accessor', function (): void {
+        expect(Response::create())
+            ->toBeInstanceOf(\Apiato\Http\Response::class);
+    });
+})->covers(Response::class);
