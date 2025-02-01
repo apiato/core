7eac6dadf3a5800b95665c76710bc0165e94d268
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-01 14:00:46 +0330

    Create DefaultProvidersTest.php

---
+++ tests/Unit/Support/DefaultProvidersTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Apiato\Console\CommandServiceProvider;
+use Apiato\Generator\GeneratorsServiceProvider;
+use Apiato\Macros\MacroServiceProvider;
+use Apiato\Support\DefaultProviders;
+
+describe(class_basename(DefaultProviders::class), function (): void {
+    it('returns the default providers', function (): void {
+        $providers = DefaultProviders::providers();
+
+        expect($providers)->toBe([
+            GeneratorsServiceProvider::class,
+            MacroServiceProvider::class,
+            CommandServiceProvider::class,
+        ]);
+    });
+})->covers(DefaultProviders::class);
