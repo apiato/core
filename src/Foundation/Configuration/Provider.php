683a713ee158c20e1cd71ee0a2e62c3bb6c7a1ab
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-01 14:25:28 +0330

    Update Provider.php

--- src/Foundation/Configuration/Provider.php
+++ src/Foundation/Configuration/Provider.php
@@ -5,14 +5,24 @@ namespace Apiato\Foundation\Configuration;
 use Apiato\Support\DefaultProviders;
 use Composer\ClassMapGenerator\ClassMapGenerator;
 use Illuminate\Support\DefaultProviders as LaravelDefaultProviders;
+use Illuminate\Support\ServiceProvider;
 
+/**
+ * @template T of ServiceProvider
+ */
 final class Provider extends LaravelDefaultProviders
 {
+    /**
+     * @param class-string<T>[]|null $providers
+     */
     public function __construct(array|null $providers = null)
     {
-        parent::__construct($providers ?: DefaultProviders::providers());
+        parent::__construct($providers ?? DefaultProviders::providers());
     }
 
+    /**
+     * @return class-string<T>[]
+     */
     public function toArray(): array
     {
         return array_unique(parent::toArray());
