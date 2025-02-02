570403f564c274e3eea14c9ff4ecd6e35cfc4e11
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-02 15:48:56 +0330

    Update ListTasks.php

--- src/Console/Commands/ListTasks.php
+++ src/Console/Commands/ListTasks.php
@@ -3,8 +3,10 @@
 namespace Apiato\Console\Commands;
 
 use Apiato\Abstract\Commands\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Symfony\Component\Finder\SplFileInfo;
 
 final class ListTasks extends Command
 {
@@ -14,19 +16,20 @@ final class ListTasks extends Command
     public function handle(): void
     {
         collect(File::allFiles(app_path()))
-            ->filter(static function (\SplFileInfo $file) {
+            ->filter(static function (SplFileInfo $file) {
                 if (Str::contains($file->getRealPath(), shared_path())) {
                     return false;
                 }
 
                 return Str::contains($file->getFilename(), 'Task.php');
-            })->groupBy(static fn (\SplFileInfo $file) => Str::of($file->getPath())
+            })->groupBy(static fn (SplFileInfo $file) => Str::of($file->getPath())
                 ->beforeLast(DIRECTORY_SEPARATOR)
                 ->afterLast(DIRECTORY_SEPARATOR)
-                ->value())->each(function ($files, $group): void {
+                ->value())->each(function (Collection $files, string $group): void {
                     $this->comment("[{$group}]");
 
-                    foreach ($files as $file) {
+                /** @var SplFileInfo $file */
+                foreach ($files as $file) {
                         $originalFileName = $file->getFilename();
                         $fileName = Str::of($originalFileName)
                             ->replace('Task.php', '')
