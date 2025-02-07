b95703710f48e6c0443575725a484f5cc2507ab9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-07 19:47:32 +0330

    Create MacroServiceProviderTest.php

---
+++ tests/Unit/Macros/MacroServiceProviderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Apiato\Macros\MacroServiceProvider;
+
+describe(class_basename(MacroServiceProvider::class), function (): void {
+    it('can search for decoded value', function (): void {
+        $hashedId = hashids()->encode(20);
+        $collection = collect([
+            ['id' => 10],
+            ['id' => 20],
+            ['id' => 30],
+        ]);
+
+        expect($collection->containsDecodedHash($hashedId))->toBeTrue();
+    });
+})->covers(MacroServiceProvider::class);
