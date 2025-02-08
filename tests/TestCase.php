ac148f1fd4ba4aecec9e431de02a2368bbbd0021
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-08 21:30:48 +0330

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,28 +5,11 @@ namespace Tests;
 use Apiato\Foundation\Apiato;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
-use Vinkla\Hashids\Facades\Hashids;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     use WithWorkbench;
     use RefreshDatabase;
-
-    public function decode(string $hashedId): int|null
-    {
-        $result = Hashids::decode($hashedId);
-
-        if (empty($result)) {
-            return null;
-        }
-
-        return $result[0];
-    }
-
-    public function encode(int $id): string
-    {
-        return Hashids::encode($id);
-    }
 
     protected function setUp(): void
     {
