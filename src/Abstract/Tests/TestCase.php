86014df7a117400ca4c9c3f45a6fdc6452be435a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-08 21:31:59 +0330

    Update TestCase.php

--- src/Abstract/Tests/TestCase.php
+++ src/Abstract/Tests/TestCase.php
@@ -4,13 +4,11 @@ namespace Apiato\Abstract\Tests;
 
 use Apiato\Support\Testing\Traits\Assertions;
 use Apiato\Support\Testing\Traits\RequestHelper;
-use Apiato\Support\Testing\Traits\TestingUser;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
 
 abstract class TestCase extends LaravelTestCase
 {
-    use TestingUser;
     use RequestHelper;
     use Assertions;
     use LazilyRefreshDatabase;
