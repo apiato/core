40317dab78ae05328d986f05e423fe6d6722e05e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-11 17:19:49 +0330

    Update TestCase.php

--- src/Abstract/Tests/TestCase.php
+++ src/Abstract/Tests/TestCase.php
@@ -3,12 +3,10 @@
 namespace Apiato\Abstract\Tests;
 
 use Apiato\Support\Testing\Traits\Assertions;
-use Apiato\Support\Testing\Traits\RequestHelper;
 use Illuminate\Foundation\Testing\TestCase as LaravelTestCase;
 
 abstract class TestCase extends LaravelTestCase
 {
-    use RequestHelper;
     use Assertions;
 
     /**
