44b866822b6cd2728a174b755c73989f697de787
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-21 13:57:12 +0330

    Update TestCaseTest.php

--- tests/Unit/Abstract/Tests/TestCaseTest.php
+++ tests/Unit/Abstract/Tests/TestCaseTest.php
@@ -4,8 +4,6 @@ namespace Tests\Unit\Abstract\Tests;
 
 use Apiato\Abstract\Tests\TestCase;
 use Apiato\Support\Testing\Traits\Assertions;
-use Apiato\Support\Testing\Traits\RequestHelper;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 beforeEach(function (): void {
     $this->sut = new class('Anonym') extends TestCase {};
@@ -13,23 +11,11 @@ beforeEach(function (): void {
 describe(class_basename(TestCase::class), function (): void {
     it('uses expected traits', function (): void {
         $traits = [
-            RequestHelper::class,
             Assertions::class,
-            LazilyRefreshDatabase::class,
         ];
 
         foreach ($traits as $trait) {
             expect(class_uses_recursive($this->sut))->toContain($trait);
         }
     });
-
-    it('can detect invalid endpoint format', function (string $endpoint): void {
-        expect(fn () => $this->sut->endpoint($endpoint)->parseEndpoint())
-            ->toThrow(\RuntimeException::class);
-    })->with([
-        '',
-        'post',
-        'get@',
-        '@users',
-    ]);
 })->covers(TestCase::class);
