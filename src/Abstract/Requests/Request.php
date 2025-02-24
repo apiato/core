38eba825df4402aa5debba0f9056737abe456dd2
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-25 02:56:50 +0330

    Update Request.php

--- src/Abstract/Requests/Request.php
+++ src/Abstract/Requests/Request.php
@@ -2,7 +2,6 @@
 
 namespace Apiato\Abstract\Requests;
 
-use Apiato\Abstract\Models\UserModel as User;
 use Illuminate\Foundation\Http\FormRequest as LaravelRequest;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
@@ -19,44 +18,11 @@ abstract class Request extends LaravelRequest
     protected array $decode = [];
 
     /**
-     * To be used mainly from unit tests.
-     */
-    public static function injectData(array $parameters = [], User|null $user = null, array $cookies = [], array $files = [], array $server = []): static
-    {
-        if (!is_null($user)) {
-            auth()->guard('api')->setUser($user);
-        }
-
-        $request = parent::create('/', 'GET', $parameters, $cookies, $files, $server);
-
-        $request->setUserResolver(static fn (): User|null => $user);
-
-        return $request;
-    }
-
-    /**
-     * Add properties to the request that are not part of the request body
-     * but are needed for the request to be processed.
-     * For example, in the unit tests, we can add the url parameters to the request which is not part of the request body.
-     * It is best used with the `injectData` method.
-     *
-     * @param array<string, mixed> $properties
-     */
-    public function withUrlParameters(array $properties): static
-    {
-        foreach ($properties as $key => $value) {
-            $this->{$key} = $value;
-        }
-
-        return $this;
-    }
-
-    /**
-     * Get the decode array.
+     * Get the "decode" property.
      *
      * @return string[]
      */
-    public function getDecodeArray(): array
+    public function getDecode(): array
     {
         return $this->decode;
     }
