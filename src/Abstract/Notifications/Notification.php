79013bf9739012e1ba18fbd7e6e434de78a6a1ab
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 19:42:58 +0330

    Update Notification.php

--- src/Abstract/Notifications/Notification.php
+++ src/Abstract/Notifications/Notification.php
@@ -6,8 +6,4 @@ use Illuminate\Notifications\Notification as LaravelNotification;
 
 abstract class Notification extends LaravelNotification
 {
-    public function via($notifiable): array
-    {
-        return config('notification.channels');
-    }
 }
