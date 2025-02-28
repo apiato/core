2f736862443d94e64df578177bbe10f6f6a022e1
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 19:42:43 +0330

    Update HasResourceKey.php

--- src/Http/Resources/HasResourceKey.php
+++ src/Http/Resources/HasResourceKey.php
@@ -6,10 +6,6 @@ trait HasResourceKey
 {
     public function getResourceKey(): string
     {
-        if (property_exists($this, 'resourceKey') && is_string($this->resourceKey)) {
-            return $this->resourceKey;
-        }
-
         return class_basename($this);
     }
 }
