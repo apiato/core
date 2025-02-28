15232a751b705ab07063b1b5acd67da588c3574e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-02-28 21:36:54 +0330

    Update EloquentCollection.php

--- src/Abstract/Collections/EloquentCollection.php
+++ src/Abstract/Collections/EloquentCollection.php
@@ -2,9 +2,16 @@
 
 namespace Apiato\Abstract\Collections;
 
+use Apiato\Abstract\Collections\EloquentCollection as AbstractEloquentCollection;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @template TKey of array-key
+ * @template TModel of Model
+ *
+ * @extends Collection<TKey, TModel>
+ *
  * @method static bool containsDecodedHash(string $hashedValue, string $key = 'id')
  */
 abstract class EloquentCollection extends Collection
