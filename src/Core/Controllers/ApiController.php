a15ad9a0cced4dd7a071d29c5ace84301cacbe19
Mohammad-Alavi <Mohammad-Alavi@users.noreply.github.com>
2025-03-03 13:59:21 +0000

    style(automated): apply php-cs-fixer rules

--- src/Core/Controllers/ApiController.php
+++ src/Core/Controllers/ApiController.php
@@ -2,13 +2,6 @@
 
 namespace Apiato\Core\Controllers;
 
-use Apiato\Core\Models\Model;
-use Apiato\Core\Transformers\Transformer;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Pagination\AbstractPaginator;
-use Illuminate\Support\Collection;
-use Spatie\Fractal\Facades\Fractal;
-
 abstract class ApiController extends Controller
 {
 }
