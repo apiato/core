6dc24f31f7bc891dbcdbbd22e8a7da353f1fbbb8
SoftHeroes <Shubhamjobanputra@gmail.com>
2025-10-12 20:44:56 +0530

    fixing DocBlock

--- src/Support/Facades/Response.php
+++ src/Support/Facades/Response.php
@@ -9,10 +9,10 @@ use League\Fractal\TransformerAbstract;
 
 /**
  * @method static \Apiato\Http\Response create(mixed $data = null, callable|TransformerAbstract|null|string $transformer = null, SerializerAbstract|null|string $serializer = null)
- * @method static JsonResponse ok()
- * @method static JsonResponse created()
- * @method static JsonResponse noContent()
- * @method static JsonResponse accepted()
+ * @method static JsonResponse ok(mixed $data = null, array $headers = [], int $options = 0)
+ * @method static JsonResponse created(mixed $data = null, array $headers = [], int $options = 0)
+ * @method static JsonResponse noContent(array $headers = [], int $options = 0)
+ * @method static JsonResponse accepted(mixed $data = null, array $headers = [], int $options = 0)
  * @method static JsonResponse respond(callable|int $statusCode = 200, callable|array $headers = [], callable|int $options = 0)
  * @method static string|callable|TransformerAbstract|null getTransformer()
  * @method static void macro(string $name, object|callable $macro)
